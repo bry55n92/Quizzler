02fb33ffcd5795c2e7e733ed295a72add1cdf801
bry55n92 <bsullivan0505@gmail.com>
2023-10-13 21:03:02 -0400

    Update to question data
    Pulled 10 new equations from open trivia API. When adding to list it only adds one.
    Will explore more tomorrow

--- data.py
+++ data.py
@@ -1,106 +1,15 @@
+import requests
+import pprint
 
+questions = requests.get(url="https://opentdb.com/api.php?amount=10&type=boolean").json()
+questions = questions["results"]
 
-
-
-question_data = [
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "The HTML5 standard was published in 2014.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "The first computer bug was formed by faulty wires.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "FLAC stands for 'Free Lossless Audio Condenser'.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "Linus Torvalds created Linux and Git.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "The programming language 'Python' is based off a modified version of 'JavaScript'",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "AMD created the first consumer 64-bit processor.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "'HTML' stands for Hypertext Markup Language.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "In most programming languages, the operator ++ is equivalent to the statement '+= 1'.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "hard",
-        "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    }
-]
+question_data = {}
+for num in range(0, len(questions)):
+    question = questions[num]["question"]
+    answer = questions[num]["correct_answer"]
+    question_data["question"] = question
+    question_data["correct_answer"] = answer
+pprint.pprint(questions)
+pprint.pprint(question_data)
+print(len(questions))
